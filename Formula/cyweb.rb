1d2d71ae53a95753764e14b2e811beba835bba27
GrumpySage <anand@adroitts.com>
2026-04-21 20:49:10 -0400

    cyweb: update sha256 from v0.1.0 release binaries

--- Formula/cyweb.rb
+++ Formula/cyweb.rb
@@ -7,22 +7,22 @@ class Cyweb < Formula
   on_macos do
     on_arm do
       url    "https://github.com/cybrium-ai/cyweb/releases/download/v0.1.0/cyweb-darwin-arm64"
-      sha256 "PLACEHOLDER"
+      sha256 "891dc12d0a35555c121898469eab9591feae103b4366bf3c6f2d3d19c8a0752c"
     end
     on_intel do
       url    "https://github.com/cybrium-ai/cyweb/releases/download/v0.1.0/cyweb-darwin-amd64"
-      sha256 "PLACEHOLDER"
+      sha256 "3fdb65d8608931f8f56893ce0d9cb7b17c89d44f55c326f637bb0305f586c5ad"
     end
   end
 
   on_linux do
     on_arm do
       url    "https://github.com/cybrium-ai/cyweb/releases/download/v0.1.0/cyweb-linux-arm64"
-      sha256 "PLACEHOLDER"
+      sha256 "6dd9d9562237bdd8b93c4eaea014e76d09df6a8a156376763130fac3f941f5e2"
     end
     on_intel do
       url    "https://github.com/cybrium-ai/cyweb/releases/download/v0.1.0/cyweb-linux-amd64"
-      sha256 "PLACEHOLDER"
+      sha256 "86067c636b28547bef2dbd20799a6371de1c3c18b94ece810e666dc3d8cad1ae"
     end
   end
 
